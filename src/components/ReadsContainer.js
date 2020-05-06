ae4dd9bf0a7b105fa1aaa682c5a0c006504e6c13
Leo Baby Jacob <33173777+leojacob7@users.noreply.github.com>
2020-05-07 00:44:31 +0530

    Update ReadsContainer.js

--- src/components/ReadsContainer.js
+++ src/components/ReadsContainer.js
@@ -27,7 +27,7 @@ class ReadsContainer extends Component {
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url( ${ book.imageLinks && book.imageLinks.thumbnail } )` }}></div>
                     <div className="book-shelf-changer">
-                    <select value={ this.state.value } onChange={ (event) => this.handleSelect( book, event.target.value ) }>
+                    <select value={ book.shelf } onChange={ (event) => this.handleSelect( book, event.target.value ) }>
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
@@ -37,7 +37,7 @@ class ReadsContainer extends Component {
                     </div>
                 </div>
                     <div className="book-title">{ book && book.title }</div>
-                    <div className="book-authors">{ book && book.authors.toString() }</div>
+                    <div className="book-authors">{ book && book.authors && book.authors.toString() }</div>
                 </div>
             </li>
         );
